a0bc3eba1a7dbcb0727aa902490843e6f23c91f2
WILLY <willy541222@example.com>
2020-11-13 22:11:50 +0800

    '123'

--- mysite/module/func.py
+++ mysite/module/func.py
@@ -113,7 +113,6 @@ def game_processing(event):
             )
             line_bot_api.reply_message(event.reply_token,message)
         else:
-            text=[]
             for i in range(len(Game_data)):
                 Game_name = Game_data[i]['ln'][0] #比賽名稱
                 player_one_chinese = Game_data[i]['atn'][0] #中文名字
@@ -136,10 +135,10 @@ def game_processing(event):
                 
                 if player_one_score != -1:
                     text3 += "當盤分數" + str(player_one_score) + " : " + str(player_two_score) + "\n"
-                text[i] = text3
-                message = TextSendMessage(text = text[i])
-                #message = [TextSendMessage(text = text3),TextSendMessage(text = "djfkoasjd")]
-            line_bot_api.reply_message(event.reply_token,message)
+        message = TextSendMessage(
+            text = text3
+        )
+        line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Error"))
 
