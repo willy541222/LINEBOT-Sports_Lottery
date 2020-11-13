6342bd3ec88cd7a1b5a9a998dd0914d6960f1648
WILLY <willy541222@example.com>
2020-11-13 23:16:15 +0800

    '123'

--- mysite/module/func.py
+++ mysite/module/func.py
@@ -113,7 +113,7 @@ def game_processing(event):
             )
             line_bot_api.reply_message(event.reply_token,message)
         else:
-            text3 = "目前場中投注的賽事"
+            text3 = "目前場中投注的賽事\n"
             for i in range(len(Game_data)):
                 Game_name = Game_data[i]['ln'][0] #比賽名稱
                 player_one_chinese = Game_data[i]['atn'][0] #中文名字
