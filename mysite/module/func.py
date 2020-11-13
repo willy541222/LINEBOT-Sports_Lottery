ba532c5df611ece455b7ddec50c9c95fb92d4548
WILLY <willy541222@example.com>
2020-11-13 22:04:21 +0800

    '123'

--- mysite/module/func.py
+++ mysite/module/func.py
@@ -135,16 +135,7 @@ def game_processing(event):
                 
                 if player_one_score != -1:
                     text3 += "當盤分數" + str(player_one_score) + " : " + str(player_two_score) + "\n"
-                message = TextSendMessage(
-                    {
-                        "type":"text",
-                        "text":"Hello, user"
-                    },
-                    {
-                        "type":"text",
-                        "text":"May I help you?"
-                    }
-                )
+                message = [TextSendMessage(text = text3),TextSendMessage(text = "djfkoasjd")
             line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Error"))
